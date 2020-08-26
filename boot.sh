f659ce249cfb6582ebdefeec2bac97fc23acfaf9
Yu Quan Chen <tylerchen@YudeMacBook-Pro.local>
2020-08-26 23:19:15 +0800

    cicd

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 
 PYTHONPATH=src CUDA_VISIBLE_DEVICES=1 python train.py \
---dataset dataset --save_every 2 --model_name=ci_training \
---restore_from=$1 --sample_length=255 --run_name=training
+--dataset dataset --save_every 200 --model_name=ci_training \
+--restore_from=$1 --run_name=training
