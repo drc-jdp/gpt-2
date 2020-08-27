0903961ac2a1065916168b3dc8dbe13bfc64c6f6
yqchenee <a6330610@gmail.com>
2020-08-27 03:09:07 +0000

    add learning rate params

--- boot.sh
+++ boot.sh
@@ -3,4 +3,8 @@
 # --dataset dataset --save_every 200 --model_name=ci_training \
 # --restore_from=$1 --run_name=training
 
-python train_mgr.py $1 --learning_rate=$2
+if [ -z "$2" ]; then
+    python train_mgr.py $1
+else
+    python train_mgr.py $1 --learning_rate=$2
+fi
