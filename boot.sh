5442039b3d093f058a265ce02c92495e2b599d49
yqchenee <a6330610@gmail.com>
2021-02-02 10:58:16 +0800

    val

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 cp models/ci_training/* /home/storage/training/
-echo $4
-python train_mgr.py $1 --learning_rate=$2 --val_dataset=$4 --save_every=$3
+python train_mgr.py $1 --learning_rate=$2 --save_every=$3 \
+    --val_dataset=$4 --val_every=$5 --val_batch_count=$6
 
